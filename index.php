249f4697fd88a27e3043eba36f4c07ea24ae93bc
Alexander <formysite.ru@yandex.ru>
2023-03-01 14:41:41 +0300

    start

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Урок 7.4. Обновление данных в таблице MySQL. Оператор UPDATE</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+</head>
+
+<body>
+    <main>
+        <header>
+            <h1>
+                Урок 7.4. Обновление данных в таблице MySQL. Оператор UPDATE
+            </h1>
+            <h4 class="subtitle">
+                Узнаем как обновить данные в таблице MySQL с помощью языка SQL и PHP
+            </h4>
+        </header>
+    </main>
+
+</body>
+
+</html>
